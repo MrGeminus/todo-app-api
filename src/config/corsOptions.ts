2e0798e878b6bc183a450af8794fabc342cb8bd8
Uros Tomic <urostomic94@gmail.com>
2022-12-21 00:02:41 +0100

    move dotenv from dev dependencies to dependencies

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -2,7 +2,7 @@ import { CorsOptions } from "cors"
 
 const corsOptions: CorsOptions = {
     origin: (origin, callback) => {
-        if (origin && `${process.env.ORIGINS}`.split(',').indexOf(origin) !== -1) callback(null, true)
+        if (origin && `${process.env.ORIGINS}`.split(',').indexOf(origin) !== -1 || !origin) callback(null, true)
         else callback(new Error('Blocked by CORS'))
     },
     credentials: true,
