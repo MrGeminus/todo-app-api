cb44441f3925cde023533c0427290672d83d7ae2
Uros Tomic <urostomic94@gmail.com>
2022-12-20 23:17:50 +0100

    test

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,8 @@ import errorHandler from "./middleware/error.middleware"
 
 dotenv.config()
 
+console.log('env')
+
 dbconnect()
 
 // Create express app
@@ -27,7 +29,7 @@ app.use(express.urlencoded({ extended: true }))
 
 // Start listening for requests on the specified port
 mongoose.connection.once('open', () => {
-    app.listen(process.env.PORT, async () => {
+    app.listen(process.env.PORT, () => {
         console.log('App running on' + process.env.PORT)
         // Register available routes
 
