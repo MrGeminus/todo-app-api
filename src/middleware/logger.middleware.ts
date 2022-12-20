2f039000fc61e221aef3b7c3d2ea4c32ed833079
Uros Tomic <urostomic94@gmail.com>
2022-12-20 20:45:22 +0100

    update log middleware

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -2,9 +2,11 @@ import { Request, Response, NextFunction } from 'express';
 import { logEvents } from '../utilities/logEvents';
 
 const logger = (request: Request, response: Response, next: NextFunction) => {
-    const message = `${request.method}\t${request.headers.origin}\t${request.url}\t`
-    const filename = 'requestLog.txt'
-    logEvents(message, filename)
+    if (process.env.NODE_ENVIRONMENT === 'production') {
+        const message = `${request.method}\t${request.headers.origin}\t${request.url}\t`
+        const filename = 'requestLog.txt'
+        logEvents(message, filename)
+    }
     next();
 }
 
