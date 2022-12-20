db89512f0aef4561312da60100765d513e3bcd63
Uros Tomic <urostomic94@gmail.com>
2022-12-20 22:56:28 +0100

    test

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -6,7 +6,7 @@ import { notFoundHandler } from '../controllers/notfound.controllers'
 const router = Router();
 
 router.use("/api/v2", userRoutes);
-router.use("/api/v2/", todoRoutes);
+router.use("/api/v2", todoRoutes);
 router.all("*", notFoundHandler);
 
 // export all routes
